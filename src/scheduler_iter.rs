2c5991e65b82f2edeb063b8fa15716e6f336ae6e
Jakub Pastuszek <jpastuszek@gmail.com>
2015-12-03 22:06:58 +0000

    it compiles

--- src/scheduler_iter.rs
+++ src/scheduler_iter.rs
@@ -1,4 +1,5 @@
 use time::{SteadyTime, Duration};
+use std::thread::sleep_ms;
 use std::collections::BTreeMap;
 use std::cmp::Ordering;
 use std::fmt;
@@ -7,7 +8,7 @@ use std::fmt::Debug;
 #[derive(Clone)]
 struct Task<Token> where Token: Clone {
     interval: Duration,
-    run_offset: SteadyTime,
+    run_offset: Duration,
     token: Token,
     bond: TaskBond
 }
@@ -28,7 +29,7 @@ impl<Token> fmt::Debug for Task<Token> where Token: Clone {
 }
 
 impl<Token> Task<Token> where Token: Clone {
-    fn new(interval: Duration, run_offset: SteadyTime, bond: TaskBond, token: Token) -> Task<Token> {
+    fn new(interval: Duration, run_offset: Duration, bond: TaskBond, token: Token) -> Task<Token> {
         assert!(interval > Duration::seconds(0)); // negative interval would make schedule go back in time!
         Task {
             interval: interval,
@@ -38,6 +39,7 @@ impl<Token> Task<Token> where Token: Clone {
         }
     }
 
+    //TODO: OneOff task should not have this
     fn next(self) -> Task<Token> {
         Task {
             run_offset: self.run_offset + self.interval,
@@ -45,14 +47,38 @@ impl<Token> Task<Token> where Token: Clone {
         }
     }
 
-    fn schedule(&self) -> SteadyTime {
+    fn schedule(&self) -> Duration {
         self.run_offset + self.interval
     }
 }
 
 pub trait TimeSource {
-    fn now(&self) -> SteadyTime;
+    // Duration since this TimeSource was crated
+    fn now(&self) -> Duration;
+    // Blocks for given duration
     fn wait(&mut self, duration: Duration);
+}
+
+struct SteadyTimeSource {
+    offset: SteadyTime
+}
+
+impl SteadyTimeSource {
+    fn new() -> SteadyTimeSource {
+        SteadyTimeSource {
+            offset: SteadyTime::now()
+        }
+    }
+}
+
+impl TimeSource for SteadyTimeSource {
+    fn now(&self) -> Duration {
+        SteadyTime::now() - self.offset
+    }
+
+    fn wait(&mut self, duration: Duration) {
+        sleep_ms(duration.num_milliseconds() as u32)
+    }
 }
 
 // TODO: should that be u64?
@@ -71,25 +97,26 @@ pub enum Schedule<Token> {
 }
 
 pub struct Scheduler<Token, TS> where TS: TimeSource, Token: Clone {
-    offset: SteadyTime,
     time_point_interval: Duration,
     tasks: BTreeMap<TimePoint, Vec<Task<Token>>>,
     time_source: TS
 }
 
 impl<Token, TS> Scheduler<Token, TS> where TS: TimeSource, Token: Clone {
+    //TODO: way to specify how many durations it is OK to consider task current
     pub fn new(time_point_interval: Duration, time_source: TS) -> Scheduler<Token, TS> where TS: TimeSource {
+        assert!(time_point_interval > Duration::seconds(0));
         Scheduler {
-            offset: time_source.now(),
             time_point_interval: time_point_interval,
             tasks: BTreeMap::new(),
             time_source: time_source
         }
     }
 
+    //TODO: with_time_source()
+
     fn schedule(&mut self, task: Task<Token>) {
         let now = self.time_source.now();
-        assert!(now > self.offset);
 
         let task = task.next();
         let time_point = self.to_time_point(task.schedule() - now);
@@ -98,23 +125,25 @@ impl<Token, TS> Scheduler<Token, TS> where TS: TimeSource, Token: Clone {
     }
 
     pub fn after(&mut self, duration: Duration, token: Token) {
-        self.schedule(Task::new(duration, SteadyTime::now(), TaskBond::OneOff, token));
+        let task = Task::new(duration, self.time_source.now(), TaskBond::OneOff, token);
+        self.schedule(task);
     }
 
     pub fn every(&mut self, duration: Duration, token: Token) {
-        self.schedule(Task::new(duration, SteadyTime::now(), TaskBond::Perpetual, token));
+        let task = Task::new(duration, self.time_source.now(), TaskBond::Perpetual, token);
+        self.schedule(task);
     }
 
     // TODO: we may miss some time points if wait waits longer
     fn next_action(&self) -> SchedulerAction {
         let now = self.time_source.now();
-        let current_time_point = self.to_time_point(now - self.offset);
+        let current_time_point = self.to_time_point(now);
 
         match self.tasks.iter().next() {
             None => SchedulerAction::None,
             Some((&time_point, _)) => {
                 match time_point.cmp(&current_time_point) {
-                    Ordering::Greater => SchedulerAction::Wait((self.offset + self.to_duration(time_point)) - now),
+                    Ordering::Greater => SchedulerAction::Wait((self.to_duration(time_point)) - now),
                     //TODO: make task Eq for sorting by time
                     Ordering::Less => SchedulerAction::Skip(self.tasks.iter().take_while(|&(&time_point, &_)| time_point < current_time_point).map(|(time_point, _)| time_point.clone()).collect()),
                     Ordering::Equal => SchedulerAction::Yield(time_point)
@@ -123,6 +152,7 @@ impl<Token, TS> Scheduler<Token, TS> where TS: TimeSource, Token: Clone {
         }
     }
 
+    //TODO: rename to next; don't wait but return NextIn(duration) or something
     pub fn wait(&mut self) -> Option<Schedule<Token>> {
         match self.next_action() {
             SchedulerAction::None => None,
@@ -182,17 +212,45 @@ impl<Token, TS> Scheduler<Token, TS> where TS: TimeSource, Token: Clone {
 
 #[cfg(test)]
 mod task {
-    use time::{SteadyTime, Duration};
-    use super::{Task, TaskBond};
+    use time::Duration;
+    use super::{Task, TaskBond, TimeSource, Scheduler};
+
+    struct MockTimeSource {
+        current_time: Duration
+    }
+
+    impl MockTimeSource {
+        fn new() -> MockTimeSource {
+            MockTimeSource {
+                current_time: Duration::seconds(0)
+            }
+        }
+    }
+
+    impl TimeSource for MockTimeSource {
+        fn now(&self) -> Duration {
+            self.current_time
+        }
+
+        fn wait(&mut self, duration: Duration) {
+            self.current_time = self.current_time + duration;
+        }
+    }
 
     #[test]
     fn getting_next_schedule() {
-        let now = SteadyTime::now();
+        let now = Duration::seconds(0);
         let interval = Duration::seconds(1);
         let task = Task::new(interval, now, TaskBond::OneOff, 42);
         println!("task: {:?}", task);
         assert_eq!(task.schedule(), now + interval);
         assert_eq!(task.next().next().schedule(), now + interval * 3);
+    }
+
+    #[test]
+    fn scheduler() {
+        let mut scheduler = Scheduler::new(Duration::seconds(1), MockTimeSource::new());
+        scheduler.after(Duration::seconds(1), 1i32);
     }
 }
 
