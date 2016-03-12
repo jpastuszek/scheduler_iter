3cf31a67040cb61e4f7cb72f05371c929571a80d
Jakub Pastuszek <jpastuszek@gmail.com>
2016-03-12 20:13:11 +0000

    abortable_wait()

--- src/scheduler/abortable_wait.rs
+++ src/scheduler/abortable_wait.rs
@@ -6,6 +6,49 @@ use time::Duration;
 use time_source::*;
 use scheduler::*;
 
+pub enum AbortableWaitError<Token> {
+    Empty,
+    Overrun(Vec<Token>),
+    Aborted
+}
+
+impl<Token> PartialEq for AbortableWaitError<Token> where Token: PartialEq<Token> {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (&AbortableWaitError::Empty, &AbortableWaitError::Empty) => true,
+            (&AbortableWaitError::Overrun(ref tokens), &AbortableWaitError::Overrun(ref other_tokens)) => tokens == other_tokens,
+            (&AbortableWaitError::Aborted, &AbortableWaitError::Aborted) => true,
+            _ => false
+        }
+    }
+}
+
+impl<Token> fmt::Debug for AbortableWaitError<Token> where Token: fmt::Debug {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            &AbortableWaitError::Empty => write!(f, "AbortableWaitError::Empty"),
+            &AbortableWaitError::Overrun(ref tokens) => write!(f, "AbortableWaitError::Overrun({:?})", tokens),
+            &AbortableWaitError::Aborted => write!(f, "AbortableWaitError::Aborted")
+        }
+    }
+}
+
+impl<Token> fmt::Display for AbortableWaitError<Token> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            &AbortableWaitError::Empty => write!(f, "scheduler is empty"),
+            &AbortableWaitError::Overrun(ref tokens) => write!(f, "scheduler overrun {} tokens", tokens.len()),
+            &AbortableWaitError::Aborted => write!(f, "wait operation was aborted from another thread")
+        }
+    }
+}
+
+impl<Token> Error for AbortableWaitError<Token> where Token: fmt::Debug + Any {
+    fn description(&self) -> &str {
+        "problem while waiting for next schedule"
+    }
+}
+
 pub enum AbortableWaitTimeoutError<Token> {
     Empty,
     Timeout,
@@ -52,9 +95,30 @@ impl<Token> Error for AbortableWaitTimeoutError<Token> where Token: fmt::Debug +
         "problem while waiting for next schedule with timeout"
     }
 }
+
 impl<Token, TS> Scheduler<Token, TS> where TS: TimeSource + Wait + AbortableWait, Token: Clone {
     pub fn abort_handle(&self) -> <TS as AbortableWait>::AbortHandle {
         self.time_source.abort_handle()
+    }
+
+    pub fn abortable_wait(&mut self) -> Result<Vec<Token>, AbortableWaitError<Token>> {
+        match self.next() {
+            Some(schedule) => match schedule {
+                Schedule::NextIn(duration) => {
+                    if let Err(WaitAbortedError) = self.time_source.abortable_wait(duration) {
+                        return Err(AbortableWaitError::Aborted);
+                    };
+                    self.abortable_wait()
+                },
+                Schedule::Overrun(overrun_tokens) => {
+                    Err(AbortableWaitError::Overrun(overrun_tokens))
+                },
+                Schedule::Current(tokens) => {
+                    Ok(tokens)
+                }
+            },
+            None => Err(AbortableWaitError::Empty)
+        }
     }
 
     pub fn abortable_wait_timeout(&mut self, timeout: Duration) -> Result<Vec<Token>, AbortableWaitTimeoutError<Token>> {
@@ -89,6 +153,27 @@ mod test {
     use scheduler::*;
     use time::Duration;
     use std::thread::spawn;
+
+    #[test]
+    fn abortable_wait_real_time() {
+        let mut scheduler = Scheduler::new(Duration::milliseconds(100));
+
+        scheduler.after(Duration::milliseconds(100), 0);
+        scheduler.after(Duration::seconds(20), 1);
+        scheduler.after(Duration::seconds(40), 2);
+
+        let abort_handle = scheduler.abort_handle();
+
+        assert_eq!(scheduler.abortable_wait(), Ok(vec![0]));
+
+        spawn(move || {
+            abort_handle.abort();
+        });
+
+        assert_eq!(scheduler.abortable_wait(), Err(AbortableWaitError::Aborted));
+    }
+
+    //TODO: more tests
 
     #[test]
     fn abortable_wait_timeout_real_time() {
